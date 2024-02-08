5dbb6ee07ca763a1ad1ed2a992d410445d0d8c02
Busappagari Shanmukha <shannub556.marolix@gmail.com>
2024-02-08 13:20:13 +0530

    sms instructions

--- djangoauthapi1/account/Sms.py
+++ djangoauthapi1/account/Sms.py
@@ -4,7 +4,7 @@ class Message:
     @staticmethod
     def send(mobile,body_text):
         account_sid = 'AC89101338654575b3281d5dc0a3a286b4'
-        auth_token = '#f0e264a1ff5b6360ad849dc246aac391' 
+        auth_token = '#f0e264a1ff5b6360ad849dc246aac391'            #Please remove # before this token after downloading this code  
         client = Client(account_sid, auth_token)
 
         message = client.messages.create(
